a1e111838ed4ebb1b9cd3c6d88aa324a1e48ff23
manish465 <ms5978307@gmail.com>
2020-02-25 23:39:40 -0800

    start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        persons: [
+            { name: "Max", age: 24 },
+            { name: "Sam", age: 22 },
+            { name: "jeff", age: 25 },
+        ],
+    };
+
+    switchNameHandler = newName => {
+        this.setState({
+            persons: [
+                { name: newName, age: 24 },
+                { name: "Sam", age: 22 },
+                { name: "jeff", age: 23 },
+            ],
+        });
+    };
+
+    onChangeHandler = event => {
+        this.setState({
+            persons: [
+                { name: "Max", age: 24 },
+                { name: event.target.value, age: 22 },
+                { name: "jeff", age: 23 },
+            ],
+        });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <button
+                    onClick={() => this.switchNameHandler("Maxinen!!!!!!!!")}
+                >
+                    Switch Names
+                </button>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}
+                    uponClick={this.switchNameHandler.bind(this, "Maxinen!!!!")}
+                />
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    changeInput={this.onChangeHandler}
+                >
+                    Hobbie: reading
+                </Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
